ba1d1eeadee431452ef5b8b059b064b3c02da01a
Caio Henrique <caiohenriquepat@gmail.com>
2025-05-28 09:02:10 -0300

    Colocando Api de cep

--- cadastrar/script.js
+++ cadastrar/script.js
@@ -1,71 +1,87 @@
+const formulario = document.getElementById("formulario");
+const cep = document.getElementById("cep");
+const mensagem = document.getElementById("mensagem");
 
-    const formulario = document.getElementById('formulario');
-    const mensagem = document.getElementById('mensagem');
+cep.onchange = function (evento) {
+  evento.preventDefault();
+  let cepValor = document.getElementById("cep").value;
 
-    
-    formulario.onsubmit = function(evento) {
-      evento.preventDefault();
+  if (cepValor.length == 8) {
+    fetch(`https://cep.awesomeapi.com.br/json/${cepValor}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        document.getElementById("Logradouro").value = data.address;
+        document.getElementById("UF").value = data.state;
+        document.getElementById("Cidade").value = data.city;
+      })
+      .catch((error) => console.log(error));
+  }
+};
 
-      const nome = document.getElementById('nome').value;
-      const email = document.getElementById('email').value;
-      const senha = document.getElementById('senha').value;
-      const datanascimento = document.getElementById('datanascimento').value;
-      const telefone = document.getElementById('telefone').value;
-      
+formulario.onsubmit = function (evento) {
+  evento.preventDefault();
 
-      
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+  const datanascimento = document.getElementById("datanascimento").value;
+  const telefone = document.getElementById("telefone").value;
 
-      if (nome === '') {
-        mensagem.textContent = 'Nome Precisa ser Preenchido.';
-        mensagem.className = 'mensagem';
-        return;
-      }
-      if (telefone === '') {
-        mensagem.textContent = 'Telefone Precisa ser Preenchido.';
-        mensagem.className = 'mensagem';
-        return;
-      }
-        if (telefone.length != 11) {
-        mensagem.textContent = 'Telefone precisa ter 11 digitos'
-        mensagem.className = 'mensagem';
-        return
-      }
-      if (email === '') {
-        mensagem.textContent = 'Email Precisa ser Preenchido.';
-        mensagem.className = 'mensagem';
-        return;
-      }
-      if (senha === '') {
-        mensagem.textContent = 'Senha Precisa ser Preenchido.';
-        mensagem.className = 'mensagem';
-        return;
-      }
-      if (datanascimento === '') {
-        mensagem.textContent = 'Data de Nascimento Precisa ser Preenchido.';
-        mensagem.className = 'mensagem';
-        return;
-      }
-      
+  if (nome === "") {
+    mensagem.textContent = "Nome Precisa ser Preenchido.";
+    mensagem.className = "mensagem";
+    return;
+  }
+  if (telefone === "") {
+    mensagem.textContent = "Telefone Precisa ser Preenchido.";
+    mensagem.className = "mensagem";
+    return;
+  }
+  if (telefone.length != 11) {
+    mensagem.textContent = "Telefone precisa ter 11 digitos";
+    mensagem.className = "mensagem";
+    return;
+  }
+  if (email === "") {
+    mensagem.textContent = "Email Precisa ser Preenchido.";
+    mensagem.className = "mensagem";
+    return;
+  }
+  if (senha === "") {
+    mensagem.textContent = "Senha Precisa ser Preenchido.";
+    mensagem.className = "mensagem";
+    return;
+  }
+  if (datanascimento === "") {
+    mensagem.textContent = "Data de Nascimento Precisa ser Preenchido.";
+    mensagem.className = "mensagem";
+    return;
+  }
 
-      
-      let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
-      const existe = usuarios.some(usuario => usuario.email === email);
-      if (existe) {
-        mensagem.textContent = 'Esse e-mail já está cadastrado.';
-        
-        return;
-      }
+  const existe = usuarios.some((usuario) => usuario.email === email);
+  if (existe) {
+    mensagem.textContent = "Esse e-mail já está cadastrado.";
 
-      usuarios.push({ nome: nome, email: email, senha: senha , datanascimento: datanascimento, telefone: telefone });
+    return;
+  }
 
-      localStorage.setItem('usuarios', JSON.stringify(usuarios));
+  usuarios.push({
+    nome: nome,
+    email: email,
+    senha: senha,
+    datanascimento: datanascimento,
+    telefone: telefone,
+  });
 
-      mensagem.textContent = 'Cadastro realizado com sucesso!';
-      mensagem.className = 'mensagem sucesso';
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
 
-      setTimeout(() => {
-            window.location.href = '../login/';
-        },2000);   
-      
-    };
+  mensagem.textContent = "Cadastro realizado com sucesso!";
+  mensagem.className = "mensagem sucesso";
+
+  setTimeout(() => {
+    window.location.href = "../login/";
+  }, 2000);
+};
